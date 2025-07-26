90aec9c549940bd8da39c783b24aba375184ed1d
michaelfuchaka <fuchakamichael06@gmail.com>
2025-07-26 22:13:34 +0300

    refactor: update age calculation test to use dynamic current year

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,17 @@
+// src/__tests__/utils.test.js
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    // Arrange
+    const currentYear = new Date().getFullYear();
     const birthYear = 1984;
+    const expectedAge = currentYear - birthYear;
+
+    // Act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+    // Assert
+    expect(ageOfPerson).toBe(expectedAge);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
-// Your code here
+
+export function currentAgeForBirthYear() {}
+
+// src/utils.js
+export function currentAgeForBirthYear(birthYear) {
+  return new Date().getFullYear() - birthYear;
+}
